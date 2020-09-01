ee943af60fa8a94c3d279efbc7bdc1c6fda461c4
Ravi-Chandra2709 <ravi48566@gmail.com>
2020-09-01 11:55:55 +0530

    first commit

---
+++ tfiles/tictactoe.js
@@ -0,0 +1,159 @@
+$("#ip1").keypress(function(event)
+{
+	var text = $(this).val();
+	if(event.which === 13)
+	{
+		 $("#span1").text(text);
+		 $("#ip1").fadeOut();
+		 var text1 = "computer";
+		 $("#span2").text(text1);
+		 $("#h2").text("vs");
+		 $("#ip2").fadeOut();
+	}
+});
+// $("#ip2").keypress(function(event)
+// {
+// 	var text = $(this).val();
+// 	if(event.which === 13)
+// 	{
+// 		 $("#span2").text(text);
+// 		 $("#h2").text("vs");
+// 		 $("#ip2").fadeOut();
+// 	}
+// });
+// var turn = 1;
+// $(".block").click(function(){
+// 	if(turn === 1)
+// 	{
+// 		$(this).text("X");
+// 		turn = 2;
+// 	}
+// 	else
+// 	{
+// 		$(this).text("O");
+// 		turn = 1;
+// 	}
+// });
+$("button").on("click",function(){
+	$(".block").text("");
+	$("input").val("");
+	$("span").text("");
+	$("#h2").text("");
+	$("#ip1").fadeIn();
+	$("#ip2").fadeIn();
+});
+
+
+
+//##########################################################//
+
+const player = "O";
+const computer = "X";
+
+let board_full = false;
+let play_board = ["", "", "", "", "", "", "", "", ""];
+
+const board_container = document.querySelector(".play-area");
+
+const winner_statement = document.getElementById("winner");
+
+check_board_complete = () => {
+  let flag = true;
+  play_board.forEach(element => {
+    if (element != player && element != computer) {
+      flag = false;
+    }
+  });
+  board_full = flag;
+};
+
+const check_line = (a, b, c) => {
+  return (
+    play_board[a] == play_board[b] &&
+    play_board[b] == play_board[c] &&
+    (play_board[a] == player || play_board[a] == computer)
+  );
+};
+
+const check_match = () => {
+  for (i = 0; i < 9; i += 3) {
+    if (check_line(i, i + 1, i + 2)) {
+      return play_board[i];
+    }
+  }
+  for (i = 0; i < 3; i++) {
+    if (check_line(i, i + 3, i + 6)) {
+      return play_board[i];
+    }
+  }
+  if (check_line(0, 4, 8)) {
+    return play_board[0];
+  }
+  if (check_line(2, 4, 6)) {
+    return play_board[2];
+  }
+  return "";
+};
+
+const check_for_winner = () => {
+  let res = check_match();
+  if (res == player) {
+    winner.innerText = "Winner is player X";
+    winner.classList.add("playerWin");
+    board_full = true
+  } else if (res == computer) {
+    winner.innerText = "Winner is computer";
+    winner.classList.add("computerWin");
+    board_full = true
+  } else if (board_full) {
+    winner.innerText = "Draw!";
+    winner.classList.add("draw");
+  }
+};
+
+const render_board = () => {
+  board_container.innerHTML = ""
+  play_board.forEach((e, i) => {
+    board_container.innerHTML += `<div id="block_${i}" class="block" onclick="addPlayerMove(${i})">${play_board[i]}</div>`
+    if (e == player || e == computer) {
+      document.querySelector(`#block_${i}`).classList.add("occupied");
+    }
+  });
+};
+
+const game_loop = () => {
+  render_board();
+  check_board_complete();
+  check_for_winner();
+}
+
+const addPlayerMove = e => {
+  if (!board_full && play_board[e] == "") {
+    play_board[e] = player;
+    game_loop();
+    addComputerMove();
+  }
+};
+
+const addComputerMove = () => {
+  if (!board_full) {
+    do {
+      selected = Math.floor(Math.random() * 9);
+    } while (play_board[selected] != "");
+    play_board[selected] = computer;
+    game_loop();
+  }
+};
+
+const reset_board = () => {
+  play_board = ["", "", "", "", "", "", "", "", ""];
+  board_full = false;
+  winner.classList.remove("playerWin");
+  winner.classList.remove("computerWin");
+  winner.classList.remove("draw");
+  winner.innerText = "";
+  render_board();
+};
+
+//initial render
+render_board();
